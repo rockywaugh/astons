c941c0f3423adb6da7dbb54bb5585873d653b3e1
Waugh, Raquache (rw756p) <rw756p@us.att.com>
2019-10-11 15:56:06 -0700

    More updates

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import ProductListing from '../features/product-listing';
+
+import data from '../data/products.json'
+
 export default function Homepage(props) {
   return <div>
     <h2>Homepage</h2>
