fb0e1942050f437ca85b336a7abb03946237393f
Waugh, Raquache (rw756p) <rw756p@us.att.com>
2019-10-11 15:59:12 -0700

    Add more files

---
+++ src/features/product-listing/product-list-item.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ProductListItem(props) {
+    return <div>
+        <h3> {props.name }</h3>
+        <img
+            height={100}
+            title={props.name}
+            src={`/products${props.image}`}
+            />
+            <div>{ props.description }</div>
+            <div>${ props.price }</div>
+            <div>
+                <button>Add to Cart</button>
+            </div>
+    </div>
+}
