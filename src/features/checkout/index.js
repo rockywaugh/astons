e2fc6a975de90d5376c68d6ddc5f73c0aa3145ab
Rocky Waugh <rocky.waugh@gmail.com>
2019-11-03 22:48:37 -0800

    initial checkout form work

--- src/features/checkout/index.js
+++ src/features/checkout/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import {connect} from 'react-redux' // {connect} is a name module
 import Cart from '../cart'
+import CheckoutForm from './form'
+import fetchApi from "../../modules/fetch-api";
+
+function submitOrder(values, cart) {
+  const { email, name } = values.order;
+
+  fetchApi('post', 'foo', {
+    order: {
+      name, // ES6 shorthand, uses the variable name as the key and variable value as the property value
+      email,
+      order_items_attributes: cart.map(item => ({
+        product_id: item.id,
+        qty: item.quantity
+      }))
+    }
+  }).then(json => {
+    if(json.errors) {
+      alert('something went wrong');
+      return
+    }
+    document.location.href = `/orders/${json.id}`
+  })
+}
 
 // Stateless component
 function Checkout(props) {
@@ -12,12 +35,14 @@ function Checkout(props) {
     <div style={{ border: '1px solid black'}}>
       <Cart/>
     </div>
+
+    <CheckoutForm onSubmit={(values) => submitOrder(values, cart)}/>
   </div>
 }
 
 function mapStateToProps(state) {
   console.log('cart index, mapStateToProps');
-  console.log(state);
+
   return {
     cart: state.cart,
   }
