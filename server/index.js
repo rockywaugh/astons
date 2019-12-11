d247453101d67a6bc0c32c265706f5cd410c485c
Rocky Waugh <rocky.waugh@gmail.com>
2019-12-11 08:07:06 -0800

    Add server

---
+++ server/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+
+// Replace if using a different env file or config
+const env = require("dotenv").config({ path: "./.env" });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+//console.log(process.env.STATIC_DIR);
+//console.log(env);
+app.use(express.static(process.env.STATIC_DIR));
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function(req, res, buf) {
+      if (req.originalUrl.startsWith("/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    }
+  })
+);
+
+app.get("/", (req, res) => {
+  // Display checkout page
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.get("/stripe-key", (req, res) => {
+  res.send({ publicKey: process.env.STRIPE_PUBLISHABLE_KEY });
+});
+
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // You should always calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+app.post("/pay", async (req, res) => {
+  const { token, items } = req.body;
+
+  const orderAmount = calculateOrderAmount(items);
+
+  try {
+    // Create a charge with the token sent by the client
+    const charge = await stripe.charges.create({
+      amount: orderAmount,
+      currency: "usd",
+      source: token
+    });
+
+    // That's it! You're done! The payment was processed 
+    res.send(charge);
+  } catch (e) {
+    // Handle "hard declines" e.g. insufficient funds, expired card, etc
+    // See https://stripe.com/docs/declines/codes for more
+    res.send({ error: e.message });
+  }
+});
+
+const portNumber = 4242;
+
+app.listen(portNumber, () => console.log(`Node server listening on port ${portNumber}!`));
