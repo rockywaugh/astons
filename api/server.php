455e2369eb083706b995f1be38842881bd6c6952
Rocky Waugh <rocky.waugh@gmail.com>
2020-01-23 01:44:46 -0800

    Update to api logic for application/json payload, addition of calculateOrderAmount logic

--- api/server.php
+++ api/server.php
@@ -3,36 +3,44 @@ require_once('vendor/autoload.php');
 
 use Stripe\Charge, Stripe\Stripe;
 
+// TODO: may not need
 $request_method = $_SERVER["REQUEST_URI"];
 
 Stripe::setApiKey('pk_test_ty0U4ZQg7NtgbvGNHb5TPYga00tifMogTU');
 
-
 // TODO: API has to run credit card purchase first and if successful,
 // submit order - adds order to database and notification sent to buyer and seller via email
 // Create helper function to calculate order amount based on data in $order payload
-function calculateOrder($order) {
-
+function calculateOrderAmount($items) {
+    $amount = 0;
+    // Error check items
+    if (!$items || !is_array($items)) {
+        return $amount;
+    }
+    // Loop through items and calculate order amount
+    foreach($items as $item) {
+        $amount = $amount + ($item["qty"] * $item["price"]);
+    }
+    return $amount;
 }
 
-var_dump($_POST);
+$_POST = json_decode(file_get_contents('php://input'), true);
 
-switch($request_method) {
-    case 'POST':
-        if (!empty($_POST["order"])) {
-            $order = $_POST["order"];
-            var_dump($order);
+if (!empty($_POST["order"])) {
 
-            // Do strip charge actions
-            /*
-            $charge = Charge::create(
-                array(
-                    'amount' => 1,
-                    'currency' => 'usd',
-                    'source' => '$token'
-                )
-            );
-            */
-        }
-        break;
+    // Get the order payload
+    $order = $_POST["order"];
+
+    // Get the order amount
+    $amount = calculateOrderAmount($order["items"]);
+
+    $charge = Charge::create(
+        array(
+            'amount' => $amount,
+            'currency' => 'usd',
+            'source' => $order["token"]
+        )
+    );
+
+    var_dump($charge);
 }
